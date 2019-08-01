d06cdae81c69584b4a82f9455ad870df14b6339f
jessica <jessica.fo003@gmail.com>
2019-08-01 15:22:23 -0300

    Criar Formulario

--- script.js
+++ script.js
@@ -3,15 +3,31 @@ let email = document.querySelector("#email");
 let confirmarEmail = document.querySelector("#confirmarEmail");
 let password = document.querySelector("#senha");
 let botao = document.querySelector("button");
+let sexo = document.querySelector("select");
+let texto = document.querySelector("h3");
+//let h1 = document.querySelector("h1");
+//let limpar = document.querySelector("#Limpar")
 
+if(email.value == confirmarEmail){
+    
+}
 
+function cadastrar(){
+    localStorage.setItem("nome", nome.value);
+    localStorage.setItem("email", email.value); 
+    localStorage.setItem("confirmarEmail", confirmarEmail.value);
+    localStorage.setItem("password", password.value);
+    localStorage.setItem("sexo", sexo.value);
 
-    function cadastrar(){
-        localStorage.setItem("nome", nome.value);
-        localStorage.setItem("email", email.value); 
-        localStorage.setItem("confirmarEmail", confirmarEmail.value);
-        localStorage.setItem("password", password.value);
-        
+    if(email.value != "" && email.value === confirmarEmail.value){
+        console.log("funcionou");
+        texto.innerHTML = ("");
     }
+    else{
+        texto.innerHTML = ("Erro em seu Email ou Email incompleto");
+        console.log("erro");
+    }
+}
 
-botao.onclick= cadastrar;
+
+botao.onclick = cadastrar;
